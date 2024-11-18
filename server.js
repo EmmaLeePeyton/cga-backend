e65ffe5ba21a57cf9285fe2d450a32cab2b6c3e9
EmmaLee Peyton <epeyton@email.sc.edu>
2024-11-18 00:33:02 -0500

    I cannot seem to get them to cooperate at this time...

--- server.js
+++ server.js
@@ -1,32 +1,65 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-app.use(cors());
+const Joi = require("joi");
+const multer = require("multer");
 app.use(express.static("public"));
+app.use("/uploads", express.static("uploads"));
+app.use(express.json());
+app.use(cors());
 
-app.get("/",()=>{
+
+const events = [
+  {
+    "name": "default",
+    "description": "No events at this time"
+  },
+  {
+    "name": "spring",
+    "description": "some events entered"
+  },
+  {
+    "name": "fall",
+    "description": "other events entered"
+  }
+];
+app.get("/",(req,res)=>{
   console.log("getting me");
-  response.sendFile(_dirname + "/index.html");
+  res.sendFile(_dirname + "/index.html");
 });
 
 app.get("/api/events", (req, res) => {
-  const events = 
-  [
-    {
-      "name": "default",
-      "description": "No events at this time"
-    },
-    {
-      "name": "spring",
-      "description": "some events entered"
-    },
-    {
-      "name": "fall",
-      "description": "other events entered"
-    }
-  ];
-  res.send(events);
+  res.json(events);
 });
+
+app.post("/api/events", (req, res) => {
+  const result = validateEvent(req.body);
+
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  const event = {
+    _id: events.length + 1,
+    name: req.body.name,
+    description: req.body.description,
+  };
+
+  events.push(event);
+  res.status(200).send(event);
+});
+
+
+const validateEvent = (event) => {
+  const schema = Joi.object({
+    _id: Joi.allow(""),
+    name: Joi.string().min(3).required(),
+    description: Joi.string().min(8).required(),
+  });
+
+  return schema.validate(event);
+};
 
 app.listen(3001, () => {
   console.log("Listening...");
