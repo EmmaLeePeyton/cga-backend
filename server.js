f3f89c34ffd0c1a7edfa822987878413778335fa
EmmaLee Peyton <epeyton@email.sc.edu>
2024-11-21 12:36:29 -0500

    post fix

--- server.js
+++ server.js
@@ -8,17 +8,31 @@ app.use("/uploads", express.static("uploads"));
 app.use(express.json());
 app.use(cors());
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/images/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
 
 const events = [
   {
+    "_id": 0,
     "name": "default",
     "description": "No events at this time"
   },
   {
+    "_id": 1,
     "name": "spring",
     "description": "some events entered"
   },
   {
+    "_id": 3,
     "name": "fall",
     "description": "other events entered"
   }
@@ -32,14 +46,17 @@ app.get("/api/events", (req, res) => {
   res.json(events);
 });
 
-app.post("/api/events", (req, res) => {
+app.post("/api/events",upload.single("img"), (req, res) => {
+  console.log("i am post");
   const result = validateEvent(req.body);
 
   if (result.error) {
+    console.log("In post error");
     res.status(400).send(result.error.details[0].message);
     return;
   }
 
+  console.log("Data is valid");
   const event = {
     _id: events.length + 1,
     name: req.body.name,
@@ -52,12 +69,13 @@ app.post("/api/events", (req, res) => {
 
 
 const validateEvent = (event) => {
+
   const schema = Joi.object({
     _id: Joi.allow(""),
     name: Joi.string().min(3).required(),
     description: Joi.string().min(8).required(),
   });
-
+  console.log(schema.validate(event))
   return schema.validate(event);
 };
 
